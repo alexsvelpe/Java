75b3c86e6d9baa27f9e8f194143245bfdaf2da24
Aleksandra Svelpe <aleksandra.svelpe@mail.ru>
2024-09-07 18:30:49 +0300

    Add files via upload

---
+++ practice1/src/ru/mirea/svelpe/Z5.java
@@ -0,0 +1,18 @@
+package ru.mirea.svelpe;
+import java.util.Scanner;
+public class Z5 {
+    public static int calculatef (int a){
+        int b = 1;
+        for(int i = 1; i <= a; i++){
+            b *= i;
+        }
+        return b;
+    }
+    public static void main(String[] args){
+        System.out.println("Введите число: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int f = calculatef(n);
+        System.out.println("Факториал числа " + n + " равен " + f);
+    }
+}
